2f650079876783bd4c9d5bd312c79529f8bb1453
Elizaveta Sh <shestakovaliza@gmail.com>
2020-11-26 19:56:52 +0700

    Add app

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webexamples',
+    'webexamples.apps.WebexamplesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/webexamples/urls.py
+++ mysite/webexamples/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import path
+from django.urls import path
 from . import views
 
 urlspatterns = [
-    path('^$', views.index, name='index')
+    path('', views.index, name='index')
 ]
