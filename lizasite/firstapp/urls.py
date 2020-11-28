a58d3bc7076d5bb5e2aeb36300ee2b6945a26aa1
Elizaveta Sh <shestakovaliza@gmail.com>
2020-11-28 21:26:19 +0700

    Data base?

--- lizasite/firstapp/urls.py
+++ lizasite/firstapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about'),
 ]
