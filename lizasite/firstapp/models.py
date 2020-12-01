38a53e4b423b2d8ef2dc4e8180cba35a0ec9fe72
Elizaveta Sh <shestakovaliza@gmail.com>
2020-12-01 19:02:56 +0700

    finish. not working

--- lizasite/firstapp/models.py
+++ lizasite/firstapp/models.py
@@ -6,4 +6,8 @@ class Task(models.Model):
     task = models.TextField('Описание')
 
     def __str__(self):
-        return self.title
+        return self.title 
+
+    class Meta:
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
--- lizasite/firstapp/views.py
+++ lizasite/firstapp/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
 
 def index(request):
-    return render(request, 'firstapp/index.html')
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'firstapp/index.html', {'title':'Главная страница сайта', 'tasks': tasks})
 
 
 def about(request):
-        return render(request, 'firstapp/about.html')
+    return render(request, 'firstapp/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST': 
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была не верной'    
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error':error,
+    }
+    return render(request, 'firstapp/create.html', context)
